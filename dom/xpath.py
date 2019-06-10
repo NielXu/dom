0ca59600b6d3dbae6c22fa377d1c6d7d86a5a161
Daniel Xu <niellxu@yahoo.ca>
2019-06-10 14:16:39 -0400

    xpath WIP

--- dom/xpath.py
+++ dom/xpath.py
@@ -1,6 +1,9 @@
 """
 XPath parser
 """
+import re
+
+
 def parse_xpath(x):
     """
     Pasing the xpath, return a list that represents
@@ -35,3 +38,39 @@ def parse_xpath(x):
     ---
     `x` The xpath in string
     """
+    q = []
+    index = 0
+    while index < len(x):
+        ch = x[index]
+        if ch == "/":
+            next_index = _find_next(index, x)
+            segment = x[index:next_index]
+            index = next_index
+            if "[" in segment:
+                _parse_cond(segment)
+            continue
+        index += 1
+
+
+def _find_next(index, x):
+    "Find the closet next path and return the index"
+    oindex = index
+    if index < len(x) - 1 and x[index+1] == "/":
+        oindex += 1
+        index += 1
+    while index < len(x):
+        ch = x[index]
+        if ch == "/" and index != oindex:
+            return index
+        index += 1
+    return index
+
+
+def _parse_cond(cond):
+    "Parse the condition in xpath"
+    print(cond)
+
+
+if __name__ == "__main__":
+    p1 = "/*//div/span[@class='className']//p/ng-button"
+    parse_xpath(p1)
